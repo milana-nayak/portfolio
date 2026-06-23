e56257b1fc85067598c5b481c8b42a64bbc7145d
milana-nayak <nayakmilana@gmail.com>
2026-06-23 12:35:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-document.getElementById("alertBtn").onclick = function () {
-  document.getElementById("contact").scrollIntoView({
-         behavior: "smooth"
-  });
-};
+const toggleButton = document.getElementById("theme-toggle");
+
+toggleButton.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark-mode");
+
+    if(document.body.classList.contains("dark-mode")){
+
+        toggleButton.textContent = "☀️";
+
+    }else{
+
+        toggleButton.textContent = "🌙";
+
+    }
+
+});
